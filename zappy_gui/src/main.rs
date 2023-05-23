00ec116a86654bbbc9ac469e02a724a2293ddbca
Aillet <arthur.aillet@epitech.eu>
2023-05-23 10:50:01 +0200

    feat(Gui): display rectangle on screen

--- zappy_gui/src/main.rs
+++ zappy_gui/src/main.rs
@@ -23,6 +23,18 @@ fn model(_app: &App) -> Model {
 fn update(_app: &App, _model: &mut Model, _update: Update) {
 }
 
-fn view(_app: &App, _model: &Model, frame: Frame){
+fn view(app: &App, _model: &Model, frame: Frame) {
     frame.clear(BLACK);
+
+    let draw = app.draw();
+
+    draw.rect()
+        .color(STEELBLUE)
+        .w(300.0)
+        .h(200.0);
+
+    match draw.to_frame(app, &frame) {
+        Err(e) => println!("error drawing to frame: {e:?}"),
+        _ => (),
+    }
 }
