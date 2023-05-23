b1cf52f95b95b0e204a7a14e74327cb8c2780d73
Aillet <arthur.aillet@epitech.eu>
2023-05-23 14:26:48 +0200

    style(Gui): remove comments

--- zappy_gui/src/main.rs
+++ zappy_gui/src/main.rs
@@ -32,13 +32,11 @@ fn update(_app: &App, model: &mut Model, update: Update) {
     let mut value2 = 0.0;
 
     egui::Window::new("Title").show(&ctx, |ui| {
-        // Resolution slider
         ui.heading("Big title");
         ui.label("Label");
         ui.add(egui::Slider::new(&mut value, 0.0..=100.0).text("My value"));
     });
     egui::Window::new("Title2").show(&ctx, |ui| {
-        // Resolution slider
         ui.heading("Big title2");
         ui.label("Label2");
         ui.add(egui::Slider::new(&mut value2, 0.0..=100.0).text("My value2"));
