193db5cbfb0c0214d590880ae976b420acd8a7a8
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2023-06-03 02:32:52 +0200

    feat(main.c): add non blocking ctrl-D and select is non blocking now

--- zappy_server/src/main.c
+++ zappy_server/src/main.c
@@ -38,13 +38,28 @@ static void set_new_loop(server_t *server, client_t *cli)
     }
 }
 
-//TODO - create a function for checking the Ctrl-D (exit the program)
+static int check_ctrl_d(void)
+{
+    int stdin_flags = fcntl(0, F_GETFL, 0);
+    fcntl(0, F_SETFL, stdin_flags | O_NONBLOCK);
+    char buf[10];
+    int ret = read(0, buf, sizeof(buf));
+    if (ret == 0 || (ret < 0 && errno != EAGAIN))
+        return 1;
+    return 0;
+}
+
 int main(int ac, char *av[])
 {
     common_t com = set_common(ac, av);
     while (1) {
+        if (check_ctrl_d() == 1)
+            break;
         set_new_loop(&com.server, com.client);
-        if (select(S_MAX + 1, &S_READ, NULL, NULL, NULL) < 0
+        struct timeval timeout;
+        timeout.tv_sec = 0;
+        timeout.tv_usec = 0;
+        if (select(S_MAX + 1, &S_READ, NULL, NULL, &timeout) < 0
         && (errno != EINTR)) {
             free_common(&com);
             fprintf(stderr, "%sSelect failed%s\n", RED, NEUTRE);
@@ -52,5 +67,6 @@ int main(int ac, char *av[])
         }
         listening_sockets(&com);
     }
+    free_common(&com);
     return 0;
 }
