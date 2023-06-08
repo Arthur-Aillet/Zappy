bf4d7a19b3a1f315f94b755068780a9134603b79
Aillet <arthur.aillet@epitech.eu>
2023-06-08 15:40:50 +0200

    feat: zappy could update separted from the rendox one

--- zappy_gui/src/main.rs
+++ zappy_gui/src/main.rs
@@ -1,18 +1,23 @@
-// use nannou::prelude::*;
-
-mod zappy;
 mod map;
 mod server;
 mod tantorian;
+mod zappy;
 
-use rend_ox::app::{app, App};
-use rend_ox::nannou::app::ModelFn;
-use rend_ox::process::{event, update};
 use crate::zappy::Zappy;
+use rend_ox::app::{app, App};
 
-// fn zappy_app()
+fn zappy_app(nannou_app: &rend_ox::nannou::App) -> App<Zappy> {
+    return app(nannou_app, Zappy::new(), zappy_update);
+}
+
+fn zappy_update(
+    _nannou_app: &rend_ox::nannou::App,
+    _zappy: &mut App<Zappy>,
+    _update: rend_ox::nannou::event::Update,
+) {
+    println!("Update!");
+}
 
 fn main() {
-    let cl = {|nannou_app : &rend_ox::nannou::App| {return app(nannou_app, Zappy::new());}};
-    rend_ox::nannou::app(cl).event(event).update(update).run();
+    rend_ox::app::launch_rendox_app(zappy_app);
 }
