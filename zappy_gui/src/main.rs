72b88ce133f46f106eaaf03d84c1274e9671388f
Aillet <arthur.aillet@epitech.eu>
2023-06-08 20:57:15 +0200

    feat(egui): basic window displaying

--- zappy_gui/src/main.rs
+++ zappy_gui/src/main.rs
@@ -5,6 +5,7 @@ mod zappy;
 
 use crate::zappy::Zappy;
 use rend_ox::app::{app, App};
+use rend_ox::nannou_egui::egui;
 
 fn zappy_app(nannou_app: &rend_ox::nannou::App) -> App<Zappy> {
     return app(nannou_app, Zappy::new(), zappy_update);
@@ -12,10 +13,29 @@ fn zappy_app(nannou_app: &rend_ox::nannou::App) -> App<Zappy> {
 
 fn zappy_update(
     _nannou_app: &rend_ox::nannou::App,
-    _zappy: &mut App<Zappy>,
-    _update: rend_ox::nannou::event::Update,
+    zappy: &mut App<Zappy>,
+    update: rend_ox::nannou::event::Update,
 ) {
-    println!("Update!");
+    let egui = &mut zappy.egui_instance;
+
+    egui.set_elapsed_time(update.since_start);
+    let ctx = egui.begin_frame();
+
+    egui::Window::new("Settings").show(&ctx, |ui| {
+        // Resolution slider
+        ui.label("Resolution:");
+
+        // Scale slider
+        ui.label("Scale:");
+
+        // Rotation slider
+        ui.label("Rotation:");
+
+        // Random color button
+        let clicked = ui.button("Random color").clicked();
+
+        if clicked {}
+    });
 }
 
 fn main() {
