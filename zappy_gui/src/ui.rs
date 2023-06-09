0b2318cb5c71d4b13b9effa2be628fcc942860a0
Aillet <arthur.aillet@epitech.eu>
2023-06-09 16:45:34 +0200

    fix(ui): better window size on communications

--- zappy_gui/src/ui.rs
+++ zappy_gui/src/ui.rs
@@ -14,12 +14,10 @@ pub(crate) fn settings(ctx: &CtxRef, camera: &mut rend_ox::camera::Camera, is_ac
 pub(crate) fn communications(ctx: &CtxRef, is_active: bool) {
     egui::Window::new("Communications")
         .enabled(!is_active)
-        .fixed_size([100., 100.])
         .show(ctx, |ui| {
             egui::ScrollArea::vertical()
                 .auto_shrink([false; 2])
                 .max_height(100.)
-                .always_show_scroll(true)
                 .stick_to_bottom()
                 .show(ui, |ui| {
                     ui.label("Test");
