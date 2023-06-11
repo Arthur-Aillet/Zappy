4d470af25e592a93768e8e958684b1eb6230cb64
syborg64 <julian.scott@epitech.eu>
2023-06-11 19:54:18 +0200

    feat(gui): added player rendering

--- zappy_gui/src/map.rs
+++ zappy_gui/src/map.rs
@@ -1,9 +1,15 @@
+use rend_ox::mesh::MeshDescriptor;
+
 pub struct Map {
     pub size: [usize; 2],
+    pub mesh: Option<MeshDescriptor>
 }
 
 impl Map {
     pub fn new() -> Map {
-        Map { size: [8, 8] }
+        Map {
+            size: [8, 8],
+            mesh: None
+        }
     }
 }
