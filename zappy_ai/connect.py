f8ea4f014341c16ad16b1302fed9d66907fa9742
Axel DENIS <axel.denis@epitech.eu>
2023-06-12 10:13:27 +0200

    doc: started doxygen doc

--- zappy_ai/connect.py
+++ zappy_ai/connect.py
@@ -1,3 +1,8 @@
+## @package connect
+# This module enable to connect to a zappy server and send messages to it.
+#
+# This modules uses sockets to connect to a zappy server and send it messages.
+
 ##
 ## EPITECH PROJECT, 2023
 ## zappy
@@ -11,6 +16,23 @@ from sys import stderr
 RETRY = 5
 
 def connect(ip, port, team_name):
+    """!
+    The function connects to a zappy server using an IP address and port number, sends a team name,
+    receives a response containing the number of places availables, the size x and y of the map,
+    and returns these values.
+
+    @param ip The IP address of the server to connect to.
+    @param port The port parameter is the port number that the client socket will use to connect to the
+    server socket.
+    @param team_name The name of the team that is connecting to the server.
+
+    @return The function `connect` returns a tuple containing a socket object (client),
+    an integer representing the client number, and the size x and y of the map. If the
+    client number is greater than or equal to 1, the client number, the places availables,
+    and the size x y of the map are returned.
+    Otherwise, everything is also returned but as the places availables is 0, the server refuses the
+    connexion.
+    """
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((ip, port))
     print("server message:", client.recv(1024))
@@ -29,6 +51,20 @@ def connect(ip, port, team_name):
     return client, 0, x, y
 
 def send_server(client, message):
+    """!
+    This function sends a message to a server and retries a certain number of times if it receives a
+    'ko' response.
+
+    @param client The client parameter is a socket object that represents a connection to a
+    server. It is used to send a message to the server and receive a response.
+    @param message The message parameter is a string that represents the message to be sent to the
+    server. It is converted to bytes using the utf-8 encoding before being sent.
+
+    @return the answer received from the server after sending a message to it. If the answer is 'ko\\n',
+    the function will retry sending the message up to a maximum number of times specified by the RETRY
+    constant. If the maximum number of retries is reached and the answer is still 'ko\\n', an error
+    message will be printed to the standard error output.
+    """
     answer = b'ko\n'
     trys = 0
     while answer == b'ko\n' and trys < RETRY:
