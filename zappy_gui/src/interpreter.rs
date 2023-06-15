e7e6652c0a5baf0b6e4eac40d7417f9ef8d6f73c
Aillet <arthur.aillet@epitech.eu>
2023-06-15 19:33:40 +0200

    feat(network): tile content command update data, and ui have a better separator

--- zappy_gui/src/interpreter.rs
+++ zappy_gui/src/interpreter.rs
@@ -9,11 +9,46 @@ pub(crate) fn create_hash_function() -> HashMap<String, ServerFunction> {
     let mut functions : HashMap<String, ServerFunction> = HashMap::new();
 
     functions.insert("msz".to_string(), Zappy::set_map_size);
+    functions.insert("bct".to_string(), Zappy::tile_content);
     functions
 }
 
 
 impl Zappy {
+    fn tile_content(&mut self, command: String) {
+        println!("{}", command);
+        let mut invalid = false;
+        let args: Vec<usize> = command.split(" ").skip(1).map(|x| -> usize {
+            return match x.parse() {
+                Ok(val) => { val }
+                Err(_) => {
+                    println!("bct: value needs to be a unsigned integers");
+                    invalid = true;
+                    0
+                }
+            }
+        }).collect();
+        if invalid == true {
+            return;
+        }
+        if args.len() != 9 {
+            println!("btc: wrong number of arguments");
+            return;
+        }
+        if args[1] > self.map.size[0] || args[0] > self.map.size[1] {
+            println!("btc: tile outside of the map");
+            return;
+        }
+        let selected_tile = &mut self.map.tiles[args[1] + self.map.size[1] * args[0]];
+        selected_tile.food = args[2];
+        selected_tile.linemate = args[3];
+        selected_tile.deraumere = args[4];
+        selected_tile.sibur = args[5];
+        selected_tile.mendiane = args[6];
+        selected_tile.phiras = args[7];
+        selected_tile.thystame = args[8];
+    }
+
     fn set_map_size(&mut self, command: String) {
         let args: Vec<&str> = command.split(" ").collect();
 
--- zappy_gui/src/ui.rs
+++ zappy_gui/src/ui.rs
@@ -96,7 +96,7 @@ impl ZappyUi {
     }
 
     pub(crate) fn tiles(&mut self, map: &crate::map::Map, is_active: bool) {
-        egui::Window::new("Map").enabled(!is_active).show(
+        egui::Window::new("Map").enabled(!is_active).auto_sized().show(
             &self.ctx.clone().expect("Ctx not set"),
             |ui| {
                 ui.add(egui::Label::new("Tiles:").heading());
@@ -108,9 +108,10 @@ impl ZappyUi {
                         self.selected_tile = self.tile_buttons(ui_grid, map);
                     });
                 if let Some(selected) = self.selected_tile {
-                    ui.add(egui::Separator::default());
+                    ui.add(egui::Label::new("__________________________________").strong());
+                    ui.add_space(10.);
 
-                    let mut title = format!("Tile {} {}: ", selected[0] + 1, selected[1] + 1);
+                    let mut title = format!("Tile {} {}: ", selected[0], selected[1]);
                     ui.add(egui::Label::new(title).heading());
                     egui::Grid::new("Tile")
                         .num_columns(2)
