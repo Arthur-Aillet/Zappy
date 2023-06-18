0e4e4d331256c6290da7266206ad479b370d3e75
Aillet <arthur.aillet@epitech.eu>
2023-06-18 20:20:11 +0200

    feat: key pressed in zappy instead of rendox

--- zappy_gui/src/main.rs
+++ zappy_gui/src/main.rs
@@ -13,8 +13,26 @@ use crate::zappy::Zappy;
 use rend_ox::app::{app, App};
 use crate::network::loop_server;
 
+fn zappy_key_pressed(app: &rend_ox::nannou::App, model: &mut App<Zappy>, key: rend_ox::nannou::event::Key) {
+    if let rend_ox::nannou::event::Key::Space = key {
+        let window = app.main_window();
+        if !model.camera_is_active {
+            if window.set_cursor_grab(true).is_ok() {
+                model.camera_is_active = true;
+            }
+        } else {
+            if window.set_cursor_grab(false).is_ok() {
+                model.camera_is_active = false;
+            }
+        }
+        window.set_cursor_visible(!model.camera_is_active);
+    }
+}
+
 fn zappy_app(nannou_app: &rend_ox::nannou::App) -> App<Zappy> {
-    let mut app = app(nannou_app, Zappy::new(), zappy::zappy_update);
+    let mut app = app(nannou_app, Zappy::new())
+        .update(zappy::zappy_update)
+        .key_pressed(zappy_key_pressed);
     Zappy::load(&mut app);
     if let Some(server) = &mut app.user.server {
         let server_clone = server.access();
