2008f5e33b8a1957c9d415a32dffdb81beb8be5a
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2023-06-23 14:52:48 +0200

    fix: fix sst command

--- zappy_server/src/commands/responses_gui/funct_server_sst.c
+++ zappy_server/src/commands/responses_gui/funct_server_sst.c
@@ -8,7 +8,7 @@
 #include "zappy.h"
 #include <string.h>
 
-void funct_server_sst(char **args, void *info, common_t *common)
+void funct_server_sst(char **args, void *info, common_t *com)
 {
     gui_t *gui = (gui_t *)info;
 
@@ -18,12 +18,10 @@ void funct_server_sst(char **args, void *info, common_t *common)
     if (gui->buffer.bufferWrite.octets == NULL) {
         return;
     }
-    common->freq = atoi(args[0]);
+    com->freq = atoi(args[0]);
     gui->buffer.bufferWrite.octets[0] = '\0';
-    strcat(gui->buffer.bufferWrite.octets, "sst ");
-    strcat(gui->buffer.bufferWrite.octets, args[0]);
-    strcat(gui->buffer.bufferWrite.octets, "\n\0");
+    sprintf(gui->buffer.bufferWrite.octets, "sst %s\n", args[0]);
     write(gui->buffer.sock.sockfd, gui->buffer.bufferWrite.octets,
-        gui->buffer.bufferWrite.usedSize);
+        strlen(gui->buffer.bufferWrite.octets));
     printf("rentrer dans la fonctions funct_server_sst\n");
 }
