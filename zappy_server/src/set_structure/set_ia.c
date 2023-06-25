0611f19e142d8204f3f07fed07b4a3099cfaba1f
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2023-06-25 10:08:34 +0200

    feat: free all msg_queue

--- zappy_server/src/set_structure/set_ia.c
+++ zappy_server/src/set_structure/set_ia.c
@@ -8,20 +8,18 @@
 #include "ia.h"
 #include <stdlib.h>
 #include "error_handling.h"
+#include "zappy.h"
 
-ia_t close_ia(void)
+void close_ia(ia_t *ia)
 {
-    ia_t ia;
-    ia.buffer.bufferRead.usedSize = 0;
-    ia.buffer.bufferRead.octets = NULL;
-    ia.buffer.bufferWrite.usedSize = 0;
-    ia.buffer.bufferWrite.octets = NULL;
-    ia.buffer.sock.sockfd = 0;
-    ia.player = NULL;
-    ia.error = NULL;
-    ia.msg_queue = NULL;
-
-    return ia;
+    ia->buffer.bufferRead.usedSize = 0;
+    ia->buffer.bufferRead.octets = NULL;
+    ia->buffer.bufferWrite.usedSize = 0;
+    ia->buffer.bufferWrite.octets = NULL;
+    ia->buffer.sock.sockfd = 0;
+    ia->player = NULL;
+    ia->error = NULL;
+    ia->msg_queue = NULL;
 }
 
 ia_t *set_ia(void)
@@ -30,7 +28,7 @@ ia_t *set_ia(void)
     if (ia == NULL)
         error("ia allocation failed", 0);
     for (int i = 0; i < MAX_PLAYER; i++) {
-        ia[i] = close_ia();
+        close_ia(&ia[i]);
     }
     if (ia == NULL)
         exit(error("Failed to allocate ia", 84));
@@ -46,6 +44,7 @@ void free_ia(ia_t *ia)
             free(ia->buffer.bufferRead.octets);
         if (ia[i].buffer.bufferWrite.octets != NULL)
             free(ia[i].buffer.bufferWrite.octets);
+        free_msg_queue(ia[i].msg_queue);
     }
     free(ia);
     basic_log("IA free", B, 0);
